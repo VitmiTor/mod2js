4be984d4e16a1610bf69db5ef5c8b5b71065514c
VitmiTor <torremvictor@gmail.com>
2023-09-24 12:03:57 -0500

    ejer2

---
+++ src/clases/Videojuegos.js
@@ -0,0 +1,50 @@
+export class Videojuegos {
+  #nombre;
+  #epoca;
+  #precio;
+  #duracion;
+  #genero;
+  #empresa;
+
+  constructor(nombre, epoca, precio, duracion, genero, empresa) {
+    this.#nombre = nombre;
+    this.#epoca = epoca;
+    this.#precio = precio;
+    this.#duracion = duracion;
+    this.#genero = genero;
+    this.#empresa = empresa;
+  }
+
+  static crearArray = (videojuegosJson) => {
+    const videojuegosArray = [];
+    videojuegosJson.forEach((element) => {
+      videojuegosArray.push(this.parsearVideojuegos(element));
+    });
+    return videojuegosArray;
+  };
+
+  static parsearVideojuegos = (videojuegosJson) => {
+    return new this(
+      videojuegosJson["nombre"],
+      videojuegosJson["epoca"],
+      videojuegosJson["precio"],
+      videojuegosJson["duracion"],
+      videojuegosJson["genero"],
+      videojuegosJson["empresa"]
+    );
+  };
+
+  mostrarInformacion() {
+    console.log(`${this.#nombre}`);
+    console.log(`${this.#epoca}`);
+    console.log(`${this.#precio}`);
+    console.log(`${this.#duracion}`);
+    console.log(`${this.#genero}`);
+    console.log(`${this.#empresa}`);
+    console.log(`---------------`);
+  }
+
+  get nombre() {
+    return this.#nombre;
+  }
+}
--- src/ejercicios/Ejercicio1.js
+++ src/ejercicios/Ejercicio1.js
@@ -1,1 +1,34 @@
-console.log("hola mundo");
+import { Peliculas } from "../clases/Peliculas.js";
+import peliculasJson from "../jsons/ex1/peliculas.json" assert { type: "json" };
+import {
+  ordenarDuracionArray,
+  ordenarGeneroArray,
+} from "../funciones/Parser.js";
+
+const peliculaArray = Peliculas.crearArray(peliculasJson);
+const peliculaTerror = [];
+const peliculaAccion = [];
+const peliculaComedia = [];
+
+ordenarGeneroArray(
+  peliculaArray,
+  peliculaTerror,
+  peliculaComedia,
+  peliculaAccion
+);
+
+ordenarDuracionArray(peliculaTerror);
+ordenarDuracionArray(peliculaComedia);
+ordenarDuracionArray(peliculaAccion);
+
+peliculaTerror.forEach((element) => {
+  element.mostrarInformacion();
+});
+
+peliculaComedia.forEach((element) => {
+  element.mostrarInformacion();
+});
+
+peliculaAccion.forEach((element) => {
+  element.mostrarInformacion();
+});
